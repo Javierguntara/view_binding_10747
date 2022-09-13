42dfd79cf81806ba0c8fdcf6c5e21fca3cd63405
Javierguntara <gondeza2003@gmail.com>
2022-09-13 11:07:49 +0900

    GD4_0747 VIEW BINDING

---
+++ app/src/main/java/com/example/view_binding_10747/Task.kt
@@ -0,0 +1,6 @@
+package com.example.view_binding_10747
+
+import android.telecom.Call
+
+data class Task(val chapter:String, val title:String,val details: String) {
+}
---
+++ app/src/main/java/com/example/view_binding_10747/TaskList.kt
@@ -0,0 +1,12 @@
+package com.example.view_binding_10747
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "TIDUR",
+        "AH MANTAP"),
+        Task("Chapter 2", "TIDUR LAGI", "Mimpi Indah"),
+        Task("Chapter 3", "MAKAN", "AYAM GEPREK"),
+        Task("Chapter 4", "TIDUR LAGI YAGESYA", "Mimpi lagi"),
+        Task("Chapter 5", "MAKAN LAGI", "Nasi Padang yagesya")
+    )
+}
